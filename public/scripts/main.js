07fe8030926457eb62887ed883bdfe298423edfc
Moses Surumen <moses.susrumen@gmail.com>
2017-11-26 00:36:43 -0800

    deploy

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,37 +1,10 @@
-/* global Barba anime hljs location _gauges */
-// ============================================================
-// Gauges
-// ============================================================
 
-var _gauges = _gauges || [];
-(function () {
-  var t = document.createElement('script')
-  t.type = 'text/javascript'
-  t.async = true
-  t.id = 'gauges-tracker'
-  t.setAttribute('data-site-id', '568ec5c0c88d9035ab000d0c')
-  t.setAttribute('data-track-path', 'https://track.gaug.es/track.gif')
-  t.src = 'https://d36ee2fcip1434.cloudfront.net/track.js'
-  var s = document.getElementsByTagName('script')[0]
-  s.parentNode.insertBefore(t, s)
-})();
 
 // ============================================================
 // Google Analytics
 // ============================================================
 
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-55019624-1', 'auto')
-ga('send', 'pageview')
-
-function gaTrack(path, title) {
-  ga('set', { page: path, title: title })
-  ga('send', 'pageview')
-}
 
 // ============================================================
 // Barba
@@ -155,24 +128,7 @@ const Home = Barba.BaseView.extend({
 // Don't forget to init the view!
 Home.init()
 
-// ============================================================
-// Document Ready
-// ============================================================
-document.addEventListener('DOMContentLoaded', function (e) {
-  Barba.Pjax.start()
-  hljs.initHighlightingOnLoad()
-})
 
 // ============================================================
 // On Page Transition
 // ============================================================
-Barba.Dispatcher.on('transitionCompleted', function (currentStatus, oldStatus, container) {
-  // Reinitialize code highlights
-  hljs.initHighlighting.called = false
-  hljs.initHighlighting()
-  console.log('Hi')
-
-  // Analytics
-  gaTrack(location.pathname, document.title)
-  _gauges.push(['track'])
-})
--- server/app.js
+++ server/app.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] :response-time ms'));
 
 // Serve static assets
-app.use(express.static(path.resolve(__dirname, '..', 'build')));
+// app.use(express.static(path.resolve(__dirname, '..', 'build')));
 
 // Always return the main index.html, so react-router render the route in the client
 app.get('*', (req, res) => {
