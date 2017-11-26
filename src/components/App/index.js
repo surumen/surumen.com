7ce51d294c1b3db8c98705414c23af5f94acd803
Moses Surumen <moses.susrumen@gmail.com>
2017-11-25 22:54:59 -0800

    deploy

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -37,51 +37,62 @@ class App extends Component {
         <div id="barba-wrapper">
           <div className="barba-container mt3" data-namespace="home">
             <div className="toc">
-              <a className="pv3 ph4 ph5-l flex items-baseline no-underline" href="/blog/facial_recognition">
-                <span className="f7 mr4 mono tracked silver language">Python</span> 
-                <span className="title dib b">Facial Recognition using Support Vector Machines and eigenfaces</span>
-              </a>
-              <a className="pv3 ph4 ph5-l flex items-baseline no-underline" href>
-                <span className="f7 mr4 mono tracked silver language">JavaScript</span> 
-                <span className="title dib b">Google Chrome weather extension</span>
-              </a>
-              <a className="pv3 ph4 ph5-l flex items-baseline no-underline" href>
-                <span className="f7 mr4 mono tracked silver language">JavaScript</span> 
-                <span className="title dib b">Maze Creator</span>
-              </a>
-              <a className="pv3 ph4 ph5-l flex items-baseline no-underline" href>
-                <span className="f7 mr4 mono tracked silver language">Java</span> 
-                <span className="title dib b">BearMaps (navigation around the Berkeley campus)</span>
-              </a>
-              <a className="pv3 ph4 ph5-l flex items-baseline no-underline" href>
-                <span className="f7 mr4 mono tracked silver language">JavaScript</span> 
-                <span className="title dib b">Free WiFi Locator</span>
-              </a>
-              <a className="pv3 ph4 ph5-l flex items-baseline no-underline" href>
-                <span className="f7 mr4 mono tracked silver language">Ruby</span> 
-                <span className="title dib b">Campus Textbook exchange</span>
-              </a>
-              <a className="pv3 ph4 ph5-l flex items-baseline no-underline" href>
-                <span className="f7 mr4 mono tracked silver language">Assembly</span> 
-                <span className="title dib b">Pyramid to Moses (Operating System)</span>
-              </a>
-              <a className="pv3 ph4 ph5-l flex items-baseline no-underline" href>
-                <span className="f7 mr4 mono tracked silver language">JavaScript</span> 
-                <span className="title dib b">RSA Encrypt</span>
-              </a>
-              <a className="pv3 ph4 ph5-l flex items-baseline no-underline" href>
-                <span className="f7 mr4 mono tracked silver language">Java</span> 
-                <span className="title dib b">Daily Sudoku Challenge</span>
-              </a>
-              <a className="pv3 ph4 ph5-l flex items-baseline no-underline" href>
-                <span className="f7 mr4 mono tracked silver language">JavaScript</span> 
-                <span className="title dib b">High School website</span>
-              </a>
-              <a className="pv3 ph4 ph5-l flex items-baseline no-underline" href>
-                <span className="f7 mr4 mono tracked silver language">JavaScript</span> 
-                <span className="title dib b">HackNairobi</span>
-              </a>
-            </div>
+                <a className="pv3 ph4 ph5-l flex items-baseline no-underline" href="/blog/facial_recognition">
+                  <span className="repo-language-color pinned-repo-meta" style={{backgroundColor: '#3572A5'}} />
+                  <span className="f7 mr4 mono tracked silver language">&nbsp;&nbsp;Python</span> 
+                  <span className="title dib b">Facial Recognition using Support Vector Machines and eigenfaces</span>
+                </a>
+                <a className="pv3 ph4 ph5-l flex items-baseline no-underline" href>
+                  <span className="repo-language-color pinned-repo-meta" style={{backgroundColor: '#5e5086'}} />
+                  <span className="f7 mr4 mono tracked silver language">&nbsp;&nbsp;JavaScript</span> 
+                  <span className="title dib b">Google Chrome weather extension</span>
+                </a>
+                <a className="pv3 ph4 ph5-l flex items-baseline no-underline" href>
+                  <span className="repo-language-color pinned-repo-meta" style={{backgroundColor: '#5e5086'}} />
+                  <span className="f7 mr4 mono tracked silver language">&nbsp;&nbsp;JavaScript</span> 
+                  <span className="title dib b">Maze Creator</span>
+                </a>
+                <a className="pv3 ph4 ph5-l flex items-baseline no-underline" href>
+                  <span className="repo-language-color pinned-repo-meta" style={{backgroundColor: '#b07219'}} />
+                  <span className="f7 mr4 mono tracked silver language">&nbsp;&nbsp;Java</span> 
+                  <span className="title dib b">BearMaps (navigation around the Berkeley campus)</span>
+                </a>
+                <a className="pv3 ph4 ph5-l flex items-baseline no-underline" href>
+                  <span className="repo-language-color pinned-repo-meta" style={{backgroundColor: '#5e5086'}} />
+                  <span className="f7 mr4 mono tracked silver language">&nbsp;&nbsp;JavaScript</span> 
+                  <span className="title dib b">Free WiFi Locator</span>
+                </a>
+                <a className="pv3 ph4 ph5-l flex items-baseline no-underline" href>
+                  <span className="repo-language-color pinned-repo-meta" style={{backgroundColor: '#701516'}} />
+                  <span className="f7 mr4 mono tracked silver language">&nbsp;&nbsp;Ruby</span> 
+                  <span className="title dib b">Campus Textbook exchange</span>
+                </a>
+                <a className="pv3 ph4 ph5-l flex items-baseline no-underline" href>
+                  <span className="repo-language-color pinned-repo-meta" style={{backgroundColor: '#6E4C13'}} />
+                  <span className="f7 mr4 mono tracked silver language">&nbsp;&nbsp;Assembly</span> 
+                  <span className="title dib b">Pyramid to Moses (Operating System)</span>
+                </a>
+                <a className="pv3 ph4 ph5-l flex items-baseline no-underline" href>
+                  <span className="repo-language-color pinned-repo-meta" style={{backgroundColor: '#5e5086'}} />
+                  <span className="f7 mr4 mono tracked silver language">&nbsp;&nbsp;JavaScript</span> 
+                  <span className="title dib b">RSA Encrypt</span>
+                </a>
+                <a className="pv3 ph4 ph5-l flex items-baseline no-underline" href>
+                  <span className="repo-language-color pinned-repo-meta" style={{backgroundColor: '#b07219'}} />
+                  <span className="f7 mr4 mono tracked silver language">&nbsp;&nbsp;Java</span> 
+                  <span className="title dib b">Daily Sudoku Challenge</span>
+                </a>
+                <a className="pv3 ph4 ph5-l flex items-baseline no-underline" href>
+                  <span className="repo-language-color pinned-repo-meta" style={{backgroundColor: '#5e5086'}} />
+                  <span className="f7 mr4 mono tracked silver language">&nbsp;&nbsp;JavaScript</span> 
+                  <span className="title dib b">High School website</span>
+                </a>
+                <a className="pv3 ph4 ph5-l flex items-baseline no-underline" href>
+                  <span className="repo-language-color pinned-repo-meta" style={{backgroundColor: '#5e5086'}} />
+                  <span className="f7 mr4 mono tracked silver language">&nbsp;&nbsp;JavaScript</span> 
+                  <span className="title dib b">HackNairobi</span>
+                </a>
+              </div>
             <div className="mh4 mh5-l pv3 pv4-l gray f7 flex-ns justify-between items-center">
               <div className="mv3 mv0-ns inline-flex items-center">
                 <a href="https://github.com/surumen" className="o-50 glow no-underline mr3">
