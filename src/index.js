efb6afb7fefe220320b2b7a7ffd11e8921b62332
Moses Surumen <moses@Mosess-MacBook-Pro.local>
2018-02-21 23:55:25 -0800

    register service worker

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {createHistory as browserHistory } from 'history/createBrowserHistory';
 
-import { registerServiceWorker } from './registerServiceWorker';
+import registerServiceWorker from './registerServiceWorker';
 import Routes from './routes';
 
 
