87729c2ddced08a568269f9fa084227c8cfb3b24
Moses Surumen <moses@Mosess-MacBook-Pro.local>
2018-02-21 23:35:47 -0800

    delte source map

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {createHistory as browserHistory } from 'history/createBrowserHistory';
 
+import { unregister } from './registerServiceWorker';
 import Routes from './routes';
 
 
@@ -9,3 +10,4 @@ ReactDOM.render(
   <Routes history={browserHistory} />,
   document.getElementById('root')
 );
+unregister();
