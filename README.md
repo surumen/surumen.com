58aff1a47446d09d70d7f215144965157fbffc55
Moses Surumen <moses@airbears2-10-142-169-19.airbears2.1918.berkeley.edu>
2018-09-06 21:39:32 -0700

    new look

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# SurumenNew
+# Surumen.Com
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.7.4.
 
--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { AppPage } from './app.po';
 
-describe('surumen-new App', () => {
+describe('surumen.com App', () => {
   let page: AppPage;
 
   beforeEach(() => {
